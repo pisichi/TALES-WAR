8228bda86f5984d0860bc2af22ac73082b1eb3f1
pisichi <win.pisichi@gmail.com>
2020-04-05 17:23:40 +0700

    add camera

---
+++ Camera.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Assignment
+{
+  class Camera
+    {
+        public Matrix Transform { get; private set; }
+
+        public void Follow(GameObject target)
+        {
+            var position = Matrix.CreateTranslation(
+              -target.Position.X - (target.Rectangle.Width / 2),
+              -target.Position.Y - (target.Rectangle.Height / 2),
+              0);
+
+            var offset = Matrix.CreateTranslation(
+                Singleton.SCREENWIDTH / 2,
+                Singleton.SCREENHEIGHT / 2,
+                0);
+
+            Transform = position * offset;
+        }
+    }
+}
